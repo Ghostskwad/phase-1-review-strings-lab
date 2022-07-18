7b1d056ff4d2db14fcd6cee1ee6ce37c6d11b992
Quade Houston <quadehouston@gmail.com>
2022-07-18 17:33:58 -0400

    commit tree 281b760d5ffe9d88c274849e46a47b4c4277c0af

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+const currentUser = 'Joe Barnes';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
